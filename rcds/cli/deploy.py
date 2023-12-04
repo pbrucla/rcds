d043d6ed691c06160c7a91ba566f375c0a07ba91
Aplet123 <aplet@aplet.me>
2023-12-03 19:58:23 -0800

    Add --no-docker flag on deploy

--- rcds/cli/deploy.py
+++ rcds/cli/deploy.py
@@ -8,7 +8,8 @@ from rcds.util import SUPPORTED_EXTENSIONS, find_files
 
 
 @click.command()
-def deploy() -> None:
+@click.option('--no-docker', '-D', is_flag=True, help="Do not build docker containers or deploy to Kubernetes.")
+def deploy(no_docker) -> None:
     try:
         project_config = find_files(["rcds"], SUPPORTED_EXTENSIONS, recurse=True)[
             "rcds"
@@ -22,20 +23,24 @@ def deploy() -> None:
     project.load_backends()
     click.echo("Loading challenges")
     project.load_all_challenges()
-    for challenge in project.challenges.values():
-        cm = rcds.challenge.docker.ContainerManager(challenge)
-        for container_name, container in cm.containers.items():
-            click.echo(f"{challenge.config['id']}: checking container {container_name}")
-            if not container.is_built():
-                click.echo(
-                    f"{challenge.config['id']}: building container {container_name}"
-                    f" ({container.get_full_tag()})"
-                )
-                container.build()
-        challenge.create_transaction().commit()
+    if not no_docker:
+        for challenge in project.challenges.values():
+            cm = rcds.challenge.docker.ContainerManager(challenge)
+            for container_name, container in cm.containers.items():
+                click.echo(f"{challenge.config['id']}: checking container {container_name}")
+                if not container.is_built():
+                    click.echo(
+                        f"{challenge.config['id']}: building container {container_name}"
+                        f" ({container.get_full_tag()})"
+                    )
+                    container.build()
+            challenge.create_transaction().commit()
     if project.container_backend is not None:
-        click.echo("Commiting container backend")
-        project.container_backend.commit()
+        if not no_docker:
+            click.echo("Commiting container backend")
+            project.container_backend.commit()
+        else:
+            click.echo("Skipping container backend because --no-docker specified")
     else:
         click.echo("WARN: no container backend, skipping...")
     if project.scoreboard_backend is not None:
